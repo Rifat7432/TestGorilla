9f897a742596a206408d0f5f2bf825c345ca301b
Rifat7432 <mdr341762@gmail.com>
2022-10-11 13:32:17 +0600

    something more added

--- src/Components/Topics/Topics.js
+++ src/Components/Topics/Topics.js
@@ -11,6 +11,7 @@ const Topics = () => {
     <div>
       <div className="top-banner w-11/12 mx-auto my-10">
         <h1 className="md:text-6xl text-3xl py-40 text-slate-300 text-center">Measure your candidates on job skills, personality and fit</h1>
+        <h1 className="md:text-6xl text-3xl py-40 text-slate-300 text-center">Choose your interview topic</h1>
       </div>
       <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6">
         {data.map((topic) => (
